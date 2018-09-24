57efc025bbe7ba2f59d1c1b12ac7386991c45849
Valeriya <valeriya.iolshina@gmail.com>
2018-09-25 01:48:54 +0300

    Iolshina_Valeriya_lr1 done

---
+++ Iolshina_Valeriya_lr1/lr1.cpp
@@ -0,0 +1,236 @@
+#include <iostream>
+#include <fstream>
+#include <cctype>
+
+using namespace std;
+
+bool isInt(ifstream &infile, char &ref);
+
+bool isUnsInt(ifstream &infile, char &ref, int i);
+
+void Error(short k);
+
+bool isNumber(ifstream &infile, char s)
+{
+	bool b;
+	if(infile >> s)
+	{
+		cout << s;
+		b = isInt(infile, s);
+		if(b)
+		{
+			if(s == '.')
+			{
+				if(infile >> s)
+				{
+					cout << s;
+					int i=0;
+					b = isUnsInt(infile, s, i);
+					if(b)
+					{
+						if (s == 'E' || s == 'e')
+						{
+							if(infile >> s)
+							{
+								cout << s;
+								b = isInt(infile, s);
+								if(b)
+									return true;
+								else
+                                                                {
+                                                                        Error(3);
+                                                                        return false; 
+                                                                }
+
+							}
+							else
+								return true;
+						}
+						else
+							return true;
+					}
+					else
+					{
+						Error(6);
+						return false;
+					}
+				}
+				else
+				{
+					Error(4);
+					return false;
+				}
+			}
+			else if (s == 'E' || s == 'e')
+			{
+				if(infile >> s)
+				{
+                                	cout << s;
+                                	b = isInt(infile, s);
+                                	if(b)
+                                                return true;
+                                        else
+                                        {
+						Error(3);
+						return false;
+					}
+                                }
+				else
+					return true;
+			}
+			else
+			{
+				Error(5);
+				return false;
+			}
+		}
+		else
+		{
+			Error(3);
+			return false;
+		}
+	}
+	else
+	{
+		Error(2);
+		return false;
+	}
+}
+
+
+bool isInt(ifstream &infile, char &ref)
+{
+	bool b;
+	int i=0;
+	if(ref == '+' || ref == '-')
+        {
+	        if(infile >> ref)
+                {
+                        cout << ref;
+                        b = isUnsInt(infile, ref, i);
+		}
+        	else return false;
+	}
+	else b = isUnsInt(infile, ref, i);
+	if(b) return true;
+	else return false;
+}
+
+bool isUnsInt(ifstream &infile, char &ref, int i)
+{
+	bool b;
+	b = isdigit(ref);
+	i++;
+	if(b)
+	{
+		if(infile >> ref)
+		{
+                	cout << ref;
+			isUnsInt(infile, ref, i);
+		}
+		else return true;
+	}
+	else if (i == 1)
+		return false;
+	else return true;
+}
+
+void Error (short k)
+{
+	cout << endl << "err#" << k << endl;
+	switch (k)
+	{
+
+		case 1: cout << "! - Лишние символы во входной строке" << endl; break;
+
+		case 2: cout << "! - Отсутствует  начальный символ" << endl; break;
+
+		case 3: cout << "! - Символ не является целым числом" << endl; break;
+
+		case 4: cout << "! - Отсутствует нужный символ" << endl; break;
+
+		case 5: cout << "! - Отсутствует точка или экспонента" << endl; break;
+
+		case 6: cout << "! - Символ не является целым беззнаковым" << endl; break;
+
+		case 7: cout << "! - Символ не является экспонентой" << endl; break;
+
+		case 8: cout << "! - " << endl; break;
+
+		default : cout << "! - ...";break;
+
+	};
+}
+
+int main()
+{
+	char s;
+	char ss[100];
+	char &ref = s;
+	bool b, exit = true;
+	int n;
+	while(exit)
+	{
+		cout << "Введите 1, если хотите ввести вещественное число" << endl << "Введите 2, если хотите использовать число из файла ex.txt" << endl << "Введите 3, чтобы завершить работу" << endl;
+        	cin >> n;
+		switch(n)
+		{
+			case 1:
+				{
+				ofstream tempfile("ex.txt");
+				cout << "Введите вещественное чило:" << endl;
+				cin >> ss;
+				tempfile << ss;
+				tempfile.close();
+				ifstream infile ("ex.txt");
+                       		if(!infile)
+                        	cout << "Входной файл не открыт" << endl;
+                        	cout << "Анализатор для вещественного числа:" << endl;
+		        	b = isNumber(infile, s);
+                        	infile >> s;
+                        	if(b && !infile.eof())
+                        	{
+                                	Error(1);
+                                	return false;
+                        	}
+                        	b = (b && infile.eof());
+                 	        infile.close();
+				if(b)
+                			cout << endl << "Это вещественное число" << endl;
+        			else
+                			cout << endl << "Это НЕ вещественное число" << endl;
+				}
+				break;
+			case 2:
+				{
+				ifstream infile ("ex.txt");
+        			if(!infile)
+                			cout << "Входной файл не открыт" << endl;
+        			cout << "Анализатор для вещественного числа:" << endl;
+        			b = isNumber(infile, s);
+        			infile >> s;
+				if(b && !infile.eof())
+        			{
+                			Error(1);
+                			return false;
+        			}
+        			b = (b && infile.eof());
+				}
+				if(b)
+                			cout << endl << "Это вещественное число" << endl;
+        			else
+                			cout << endl << "Это НЕ вещественное число" << endl;
+				break;
+			case 3:
+				{
+				exit = false;
+				return 0;
+				}
+			default:
+				cout << "Повторите попытку" << endl;
+				break;
+		}
+	}
+return 0;
+}
+
